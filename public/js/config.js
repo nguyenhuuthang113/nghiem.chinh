303c15e0954f1db465e703c064691cb6e14dff00
nguyenhuuthang113 <huuthang030603@gmail.com>
2021-11-04 19:41:45 +0700

    oik

---
+++ public/js/config.js
@@ -0,0 +1,60 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'NGHIÊM K2',
+        'favicon' : 'home.png',
+        'notice': '🤗Welcome To Visit My Website😘'
+    },
+    'cover-image' : 'ItsukishimaGateJapan-day.gif',
+    'avatar-image' : 'avatar.jpg',
+    'name' : 'Đăng Nghiêm', 
+    'is_tick' : true,
+    'skills' : [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'admin.png',
+            'content' : 'CONTACT ME👇',
+            'author' : 'H.V.M Đăng Nghiêm',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Billing Information', 
+            'show-value' : 'Đăng Nghiêm',
+            'value' : 'http://nghiemcopyright.xyz/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'My Facebook', 
+            'show-value' : 'Contact Me Via Facebook' ,
+            'value' : 'https://www.facebook.com/NGHIEMK2',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'My Zalo', 
+            'show-value' : 'Contact Me Via Zalo',
+            'value' : 'https://zalo.me/0979603297',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : 'Contact Me By Phone Number',
+            'value' : 'tel:0979603297',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'Contact Me Via Gmail',
+            'value' : 'mailto:namlinh215@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
